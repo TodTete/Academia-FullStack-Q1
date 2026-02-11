a3d769059011ea473fe41ff5ea50d1ca0b9adbac
TodTete <vallejoricardo3@gmail.com>
2026-02-11 17:24:14 -0600

    Ejercicio 1.1

---
+++ src/com/bootcamp/week1/ejercicios/Ejecicio_1.java
@@ -0,0 +1,24 @@
+package com.bootcamp.week1.ejercicios;
+
+public class Ejecicio_1 {
+    public static void main(String[] args){
+        //TODO: Declarar variables
+        String nombre = "Ricardo Vallejo";
+        int edad = 22;
+        double altura = 1.67;
+        boolean esActivo = true;
+
+        //TODO: Concatenacion con +
+        String mensaje1 = "Me llamo " + nombre + ", tengo " + edad
+                          + " anios, mido " + altura + "m y estoy "
+                          + (esActivo ? "activo" : "inactivo") + ".";
+        System.out.println(mensaje1);
+
+        //TODO: Usando String.format()
+        String mensaje2 = String.format(
+                "Me llamo %s, tengo %d anios, mido %.2fm y estoy %s.",
+                nombre, edad, altura, esActivo ? "activo" : "inactivo"
+        );
+        System.out.println(mensaje2);
+    }
+}
