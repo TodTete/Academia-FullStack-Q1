5c7f181b03a69829297768b621475b050739b389
TodTete <vallejoricardo3@gmail.com>
2026-03-01 10:56:15 -0600

    Ejercicio JV17 #8

---
+++ src/p1/cuatro.java
@@ -0,0 +1,12 @@
+package p1;
+
+public class cuatro {
+    public static void main(String[] args) {
+        String s = "10_00";
+        Integer s2 = 10_00;
+        //Line n1
+        Integer res = 250 + s2;
+        //Integer res = 250 + Integer.parseInt(s);
+        System.out.println(res);
+    }
+}
