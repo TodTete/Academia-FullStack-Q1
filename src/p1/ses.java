04c8c011d55e033207029b814d1c38e90e33b33d
TodTete <vallejoricardo3@gmail.com>
2026-03-01 11:29:12 -0600

    Ejercicio JV17 #18

---
+++ src/p1/ses.java
@@ -0,0 +1,9 @@
+package p1;
+
+public class ses {
+    void print(int i){System.out.println("hello");}
+    void print(long j){System.out.println("there");}
+    public static void main(String[] args) {
+        new ses().print(0b1101_1010);
+    }
+}
