f5bad45f83df18131e6d84fd92d3cedf487a0ae3
TodTete <vallejoricardo3@gmail.com>
2026-03-01 10:55:52 -0600

    Ejercicio JV17 #6

---
+++ src/p1/App2.java
@@ -0,0 +1,17 @@
+package p1;
+
+class A { public void mA() {System.out.println("mA");} }
+class B extends A { public void mB() {System.out.println("mB");}}
+class C extends B { public void mC() {System.out.println("mC");}}
+public class App2 {
+    public static void main(String[] args) {
+        A bobj = new B();
+        A cobj = new C();
+        if (cobj instanceof B v) {
+            v.mB();
+            if (v instanceof C v1){ v1.mC(); }
+        } else{
+            cobj.mA();
+        }
+    }
+}
