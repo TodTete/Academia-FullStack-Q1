c465fc0799b2cc05b81b61b43d2703d47703fdf2
TodTete <vallejoricardo3@gmail.com>
2026-02-28 23:02:21 -0600

    Ejercicio JV17 #4

--- src/com/bootcamp/week4/main.java
+++ src/com/bootcamp/week4/main.java
@@ -1,4 +1,18 @@
 package com.bootcamp.week4;
 
+class ZeroValueException extends Exception{}
+
 public class main {
+    public static void main(String[] args) {
+        int a[] = {0,2,4};
+        try{
+            try {
+                if (a[0] == 0) throw new ZeroValueException();
+                }catch (ZeroValueException  | NullPointerException zve) {
+                throw zve;
+                }
+            }catch (Exception e){
+                System.out.println("Caught " + e);
+            }
+    }
 }
