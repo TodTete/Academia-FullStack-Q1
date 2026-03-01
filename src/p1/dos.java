4c54945e246bd2d29f12837a199e7c53956c33ee
TodTete <vallejoricardo3@gmail.com>
2026-02-28 23:01:34 -0600

    Ejercicio JV17

---
+++ src/p1/dos.java
@@ -0,0 +1,9 @@
+package p1;
+
+public class dos {
+    public static void main(String[] args) {
+        float x = Math.round((1.0/3)*100)/10f/(((int) 'a' == 0Xa) ?
+        0 : 3);
+            System.out.println(x);
+    }
+}
