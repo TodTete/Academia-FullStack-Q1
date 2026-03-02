1683588f42dee18b3ce3f37ef8ee00a2c393e096
TodTete <vallejoricardo3@gmail.com>
2026-03-02 13:01:33 -0600

    Delete forever

--- src/p1/ses.java
+++
@@ -1,9 +0,0 @@
-package p1;
-
-public class ses {
-    void print(int i){System.out.println("hello");}
-    void print(long j){System.out.println("there");}
-    public static void main(String[] args) {
-        new ses().print(0b1101_1010);
-    }
-}
