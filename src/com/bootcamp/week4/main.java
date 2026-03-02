f9be7fd172a6c3e2b13e6fb8b2c6ea524dae1b99
TodTete <vallejoricardo3@gmail.com>
2026-03-02 16:40:26 -0600

    APX

--- src/com/bootcamp/week4/main.java
+++ src/com/bootcamp/week4/main.java
@@ -1,18 +1,20 @@
 package com.bootcamp.week4;
 
-class ZeroValueException extends Exception{}
+// el <T> es un tipo generico, se puede usar cualquier tipo de dato
+class Transaction<T>{
+    private T amount;
+
+    public Transaction(T amount) {
+        this.amount = amount;
+    }
+
+    public T getAmount() {
+        return amount;
+    }
+}
 
 public class main {
     public static void main(String[] args) {
-        int a[] = {0,2,4};
-        try{
-            try {
-                if (a[0] == 0) throw new ZeroValueException();
-                }catch (ZeroValueException  | NullPointerException zve) {
-                throw zve;
-                }
-            }catch (Exception e){
-                System.out.println("Caught " + e);
-            }
+
     }
 }
